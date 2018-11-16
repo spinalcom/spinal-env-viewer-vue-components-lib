a79e667b8806fd7fe18b6c78ec5a08a1009aa1a2
Loris Ciprin <l.ciprin@spinalcom.com>
2018-11-16 16:24:17 +0100

    "improve css"

--- components/Nodes/NodeHeader.vue
+++ components/Nodes/NodeHeader.vue
@@ -1,20 +1,23 @@
 <template>
-    <div class="node-header" @click="$emit('node-selected')">
+    <div class="node-header"
+         @click="$emit('node-selected', [nodeId])">
         <drop-up-down-button
+                class="node-list-icon"
                 :opened="opened"
-                :click="displayChildren"
-                v-if="hasChild"
+                @click="$emit('toggle-display-child')"
+                :style="{visibility: isVisible}"
         />
-        <div class="name">
-            <h3> {{info.name}}</h3>
+        <div class="node-name">
+            {{name}}
         </div>
-        <base-icon-button
-                class="item"
-                :button-info="{icon: 'remove_red_eye', toolTip: '', icon_type: 'in', fontColor: '#FFF', backgroundColor: '#ff191c' }"
-                :click="hideBimObject"
-        />
+        <i class="material-icons node-hide-bim"
+           @click="hideBimObj()"
+        >
+            remove_red_eye
+        </i>
         <color-maker
-                :color="info.color"
+                class="node-color"
+                :color="color"
         />
     </div>
 </template>
@@ -28,44 +31,111 @@
         name: "NodeHeader",
         components: {ColorMaker, BaseIconButton, DropUpDownButton},
         props: {
-            opened: {
-                type: Boolean,
-                required: true
-            },
-            displayChildren: {
-                type: Function,
-                required: true
-            },
             hasChild: {
                 type: Boolean,
                 required: true
             },
+
             info: {
                 type: Object,
                 required: true
             },
-            hideBimObject: {
-                type: Function,
-                required: true,
+
+            nodeId: {
+                type: String,
+                required: true
+            },
+
+            opened: {
+                type: Boolean,
+                required: true
+            }
+        },
+        watch: {
+            hasChild: {
+                handler: function (oldValue, newValue) {
+                    if (!newValue) {
+                        this.isVisible = "";
+                    }
+                    else {
+                        this.isVisible = "";
+                    }
+                }
+                ,
+                immediate: true
+            },
+            info: {
+                handler: function (oldValue, newValue) {
+                    if (typeof newValue !== "undefined") {
+                        if (newValue.hasOwnProperty("name"))
+                            this.name = newValue.name.get();
+                        if (newValue.hasOwnProperty("color"))
+                            this.color = newValue.color.get();
+                    }
+
+                    else if (typeof oldValue !== "undefined") {
+                        if (oldValue.hasOwnProperty("name"))
+                            this.name = oldValue.name.get();
+                        if (oldValue.hasOwnProperty("color"))
+                            this.color = oldValue.color.get();
+                    }
+                    else {
+                        this.name = "";
+                        this.color = "";
+                    }
+                },
+                immediate: true
+
+            }
+        },
+        methods: {
+            hideBimObj: function () {
+                this.$emit('hide-bim-object', [this.nodeId]);
             }
         }
     }
 </script>
 
 <style scoped>
+
     .node-header {
-        display: grid;
-        grid-template-columns: auto auto auto auto;
+        position: relative;
         width: 100%;
+        height: 36px;
+        padding: 9px 6px 6px 13px;
     }
 
-    .item {
-        text-align: center;
+    .node-header:hover {
+        background-color: rgba(0, 0, 0, 0.3);
+        cursor: pointer;
+        padding: 9px 6px 6px 13px;
     }
 
-    .name {
-
-        width: 50%;
+    .node-list-icon {
+        position: absolute;
+        left: 0;
+        width: 21px;
     }
+
+    .node-name {
+        position: absolute;
+        left: 21px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: calc(100% - 77px);
+    }
+
+    .node-hide-bim {
+        position: absolute;
+        right: 15px;
+        top: 6px;
+    }
+
+    .node-color {
+        position: absolute;
+        right: 0;
+    }
+
 
 </style>
--- components/Nodes/NodeItem.vue
+++ components/Nodes/NodeItem.vue
@@ -1,20 +1,30 @@
 <template>
-    <div>
+    <ul class="node-item">
         <node-header
-                class="node-header"
-                :has-child="children.length > 0"
-                :display-children="displayChildren"
-                :hide-bim-object="hideBIMObject"
-                :info="info"
+                v-if="nodes.hasOwnProperty(nodeId)"
+                :has-child="childrenIds.length > 0"
+                :info="nodes[nodeId].info"
                 :opened="opened"
-                @node-selected="$emit('node-selected', context)"
+                :node-id="nodeId"
+                @hide-bim-object="$emit('hide-bim-object',$event)"
+                @node-selected="$emit('node-selected', $event)"
+                @toggle-display-child="displayChildren()"
         />
-        <ul class="children" v-if="opened">
-            <li v-for="child in children">
-                <node-item :info="child.info" :children="child.children" :context="child.context" @node-selected="$emit('node-selected', child.context)"/>
-            </li>
-        </ul>
-    </div>
+
+        <li
+                v-for="(child, index) in childrenIds"
+                v-if="opened"
+                :key="index"
+        >
+            <node-item
+                    :nodes="nodes"
+                    :node-id="child"
+                    @node-selected="onNodeSelected($event)"
+                    @pull-node="pullNode($event)"
+            />
+        </li>
+    </ul>
+
 </template>
 
 <script>
@@ -29,60 +39,90 @@
         components: {NodeHeader, ColorMaker, BaseIconButton, DropUpDownButton},
         data: function () {
             return {
-                opened: false
+                opened: false,
+                childrenIds: [],
+                node: {}
             }
         },
 
         props: {
-            info: {
+            nodes: {
                 type: Object,
                 required: true,
             },
-            context: {
-                type: Object,
+            nodeId: {
+                type: String,
                 required: true
-            },
-            children: {
-                type: Array,
-                default: function () {
-                    return [];
-                }
             }
         },
 
         methods: {
             displayChildren: function () {
                 this.opened = !this.opened;
-
             },
-            hideBIMObject: function () {
-                console.log("hide bim object");
+            pullNode: function (event) {
+                if (typeof event === "undefined")
+                    this.$emit('pull-node', this.nodeId);
+                else
+                    this.$emit('pull-node', event);
             },
-            logChild: function (child) {
-                console.log("vue child", child)
-
-            },
-            onSelectedNode: function () {
-                this.onClick(this.context);
+            onNodeSelected: function (event) {
+                if (typeof event === "undefined")
+                    this.$emit('node-selected', [this.nodeId]);
+                else
+                {
+                    event.push(this.nodeId);
+                    this.$emit('node-selected', event);
+                }
             }
         },
 
+        watch: {
+            nodes: {
+                handler: function (oldValue, newValue) {
+
+                    if ( typeof newValue !== "undefined"
+                        && newValue.hasOwnProperty(this.nodeId)
+                    ) {
+                        this.node = newValue[this.nodeId];
+                        this.childrenIds = this.node.getChildrenIds();
+                    }
+                    else if ( typeof oldValue !== "undefined"
+                        && oldValue.hasOwnProperty(this.nodeId)
+                    ) {
+                        this.node = oldValue[this.nodeId];
+                        const childrenIds = this.node.getChildrenIds();
+
+                        for (let i = 0; i < childrenIds.length; i++) {
+                            this.childrenIds.push(childrenIds[i]);
+                        }
+                    }
+                    else {
+                        this.node = {};
+                        this.childrenIds = [];
+                    }
+                },
+                immediate: true
+            }
+        },
+        mounted: function () {
+            if (!this.nodes.hasOwnProperty(this.nodeId)){
+                this.$emit("pull-node");
+            }
+
+        }
 
     }
 </script>
 
 <style scoped>
 
-    #hideObjectEye {
-        float: left;
-    }
 
-    ul {
+    .node-item {
         width: 100%;
         list-style-type: none;
+        padding-inline-start: 0px;
     }
 
-    .node-header {
-        border: 2px solid;
-    }
+
 </style>
