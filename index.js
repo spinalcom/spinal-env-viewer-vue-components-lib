de5caae83ffea2365cf7cb7129378abd52f4fc31
LorisC <loris@ciprin.fr>
2019-01-04 15:58:25 +0100

    [UPDATE]

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 /*
- * Copyright 2018. SpinalCom - www.spinalcom.com
+ * Copyright 2019 SpinalCom - www.spinalcom.com
  *
- * This file is part of SpinalCore.
- * Please read all of the following terms and conditions
- * of the Free Software license Agreement ("Agreement")
- * carefully.
- * This Agreement is a legally binding contract between
- * the Licensee (as defined below) and SpinalCom that
- * sets forth the terms and conditions that govern your
- * use of the Program. By installing and/or using the
- * Program, you agree to abide by all the terms and
- * conditions stated or referenced herein.
- * If you do not agree to abide by these terms and
- * conditions, do not demonstrate your acceptance and do
- * not install or use the Program.
- * You should have received a copy of the license along
- * with this file. If not, see
- * <http://resources.spinalcom.com/licenses.pdf>.
+ *  This file is part of SpinalCore.
  *
+ *  Please read all of the following terms and conditions
+ *  of the Free Software license Agreement ("Agreement")
+ *  carefully.
  *
+ *  This Agreement is a legally binding contract between
+ *  the Licensee (as defined below) and SpinalCom that
+ *  sets forth the terms and conditions that govern your
+ *  use of the Program. By installing and/or using the
+ *  Program, you agree to abide by all the terms and
+ *  conditions stated or referenced herein.
+ *
+ *  If you do not agree to abide by these terms and
+ *  conditions, do not demonstrate your acceptance and do
+ *  not install or use the Program.
+ *  You should have received a copy of the license along
+ *  with this file. If not, see
+ *  <http://resources.spinalcom.com/licenses.pdf>.
  */
 
-import {SpinalBadgeIconButton, DropUpDownButton, SpinalIconButton} from "./components/Button";
+import {
+  DropUpDownButton,
+  SpinalBadgeIconButton,
+  SpinalIconButton
+} from "./components/Button";
 import ColorMarker from "./components/ColorMarker/ColorMaker.vue";
-import {NodeList, ChildInspector, NodeHeader, NodeItem} from "./components/Nodes";
-import {ToolsBar, SideBar, TopBar} from "./components/ToolsBar";
+import {
+  ChildInspector,
+  NodeHeader,
+  NodeItem,
+  NodeList
+} from "./components/Nodes";
+import {
+  SideBar,
+  ToolsBar,
+  TopBar
+} from "./components/ToolsBar";
 
 
 export {
-    SpinalBadgeIconButton,
-    DropUpDownButton,
-    SpinalIconButton,
-    ColorMarker,
-    NodeList,
-    ChildInspector,
-    NodeHeader,
-    NodeItem,
-    SideBar,
-    TopBar,
-    ToolsBar
-}
+  SpinalBadgeIconButton,
+  DropUpDownButton,
+  SpinalIconButton,
+  ColorMarker,
+  NodeList,
+  ChildInspector,
+  NodeHeader,
+  NodeItem,
+  SideBar,
+  TopBar,
+  ToolsBar
+};
