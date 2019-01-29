f698dd09a27fe8c57a253208f940eecdb3c32361
LorisC <loris@ciprin.fr>
2019-01-29 11:24:47 +0100

    revert

--- components/Nodes/ChildInspector.vue
+++ components/Nodes/ChildInspector.vue
@@ -25,6 +25,7 @@
 <template>
     <div class="child-inspector">
         <div class="child-inspector-name">
+<<<<<<< HEAD
             {{name}}
         </div>
         <md-field v-if="relationNames.length > 0">
@@ -33,10 +34,19 @@
                 <md-option :key="index" :value="name" v-for="(name, index) in relationNames">{{name}}</md-option>
             </md-select>
         </md-field>
+=======
+            <h1>
+                {{name}}
+            </h1>
+        </div>
+>>>>>>> parent of 0541960... [UPDATE] globe improvement
 
-        <div :key="index" class="child-inspector-child" v-for="(info, index) in childInfo">
 
+        <div class="child-inspector-child"
+             v-for="(info, index) in childInfo"
+             :key="index">
             <div class="child-inspector-child-name">
+<<<<<<< HEAD
                     {{info.name.get()}}
             </div>
 
@@ -49,7 +59,19 @@
                 </i>
 
             </div>
+=======
+                <h2>
+                    {{info.name}}
+                </h2>
+            </div>
 
+>>>>>>> parent of 0541960... [UPDATE] globe improvement
+
+            <spinal-icon-button
+                    class="child-inspector-delete-child"
+                    icon="delete"
+                    tool-tip="suppress child"
+            />
         </div>
 
 
@@ -57,6 +79,7 @@
 </template>
 
 <script>
+<<<<<<< HEAD
   import NodeHeader from "./NodeHeader.vue";
   import SpinalIconButton from "../Button/SpinalIconButton.vue";
   import deleteForEvener from './remove-forever.svg'
@@ -102,19 +125,61 @@
         }
       }
     }
+=======
+    import NodeHeader from "./NodeHeader.vue";
+    import SpinalIconButton from "../Button/SpinalIconButton.vue";
 
-  }
+    export default {
+        name: "ChildInspector",
+        components: {SpinalIconButton, NodeHeader},
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            childInfo: {
+                type: Array,
+                default: function () {
+                    return [
+                        {name: "child 1"},
+                        {name: "child 2"},
+                        {name: "child 3"},
+                        {name: "child 4"}
+                    ];
+                }
+            },
+            defaultRelationName: {
+                type: String,
+                default: function () {
+                    return ""
+                }
+            }
+        },
+        data: function () {
+            return {
+                relationName: "hah"
+            }
+        },
+        mounted() {
+            this.relationName = this.defaultRelationName;
+        }
+>>>>>>> parent of 0541960... [UPDATE] globe improvement
+
+    }
 </script>
 
 <style scoped>
 
+<<<<<<< HEAD
     .child-inspector{
         width: 50%;
     }
 
+=======
+>>>>>>> parent of 0541960... [UPDATE] globe improvement
     .child-inspector-name {
-        text-align: center;
         height: 30px;
+<<<<<<< HEAD
         text-overflow: ellipsis;
         white-space: nowrap;
         overflow: hidden;
@@ -128,6 +193,12 @@
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
+=======
+    }
+
+    .child-inspector-child-name {
+        height: 55px;
+>>>>>>> parent of 0541960... [UPDATE] globe improvement
     }
 
     .child-inspector-child {
@@ -137,11 +208,15 @@
         height: 30px;
     }
 
-    .child-inspector-child-delete-buttons {
+    .child-inspector-delete-child {
         position: absolute;
         right: 0;
         top: 5px;
+<<<<<<< HEAD
         display: flex;
+=======
+>>>>>>> parent of 0541960... [UPDATE] globe improvement
     }
+
 
 </style>
